f2ef0f50293a512ba6dc94d52c6059cd06a0ed33
Luís Olivieri <116577191+LuisOLivieri@users.noreply.github.com>
2023-10-12 08:40:23 -0300

    Add files via upload

---
+++ TesteLampada.java
@@ -0,0 +1,17 @@
+package PactLampada;
+
+public class TesteLampada {
+
+	public static void main(String[] args) {
+		Lampada lamp = new Lampada();
+		
+		lamp.ligar();
+		lamp.mostrarEstado();
+		lamp.desligar();
+		lamp.mostrarEstado();
+		lamp.mudarEstado();
+		lamp.mostrarEstado();
+
+	}
+
+}
